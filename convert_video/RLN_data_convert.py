04d55115194ca7327d982d37988fe59ee03477f2
Zhong Cenzhe <qe54089718@gmail.com>
2020-11-04 00:24:27 +0800

    convert&createALL_video_folder

---
+++ convert_video/RLN_data_convert.py
@@ -0,0 +1,25 @@
+import os, sys
+from glob import glob
+from my_package.module_convert_a import convert
+from tqdm import tqdm
+#讀檔建立資料夾
+mp4_files = glob(os.path.join(r"D:/Medical_Imaging_Project/RLN_183/video", "*.mp4"))
+path = "D:/Medical_Imaging_Project/RLN_183/RSLN_video_data_{}"#資夾名稱D:\Medical_Imaging_Project\RLN_183
+i=0#計算資料夾編號
+print(len(mp4_files))
+
+for mp4_file in tqdm(mp4_files):
+    print(mp4_file)
+# 創建目錄
+
+    i+=1#資料夾編號
+    #print(i)
+    folder=path.format(i)#串接資料夾編號名字
+    #os.mkdir(folder, 755 )#創建目錄
+    os.mkdir(folder)#創建目錄
+    
+    convert(mp4_file,folder)
+    print("目錄創建_ok"+str(i))
+    print("convert_ok"+str(i))
+print("目錄創建_ok_ALL")
+print("convert_ok_ALL")
---
+++ convert_video/RSLN_data_convert.py
@@ -0,0 +1,27 @@
+import os, sys
+from glob import glob
+from my_package.module_convert_a import convert
+from tqdm import tqdm
+import cv2
+#讀檔建立資料夾
+mp4_files = glob(os.path.join(r"D:/Medical_Imaging_Project/RSLN_45/video", "*.mp4"))
+path = "D:/Medical_Imaging_Project/RSLN_45/RSLN_video_data_{}"#資夾名稱
+#D:\Medical_Imaging_Project\RSLN_45\video
+i=0#計算資料夾編號
+print(len(mp4_files))
+
+for mp4_file in tqdm(mp4_files):
+    print(mp4_file)
+# 創建目錄
+
+    i+=1#資料夾編號
+    #print(i)
+    folder=path.format(i)#串接資料夾編號名字
+    #os.mkdir(folder, 755 )#創建目錄
+    os.mkdir(folder)#創建目錄
+    
+    convert(mp4_file,folder)
+    print("目錄創建_ok"+str(i))
+    print("convert_ok"+str(i))
+print("目錄創建_ok_ALL")
+print("convert_ok_ALL")
